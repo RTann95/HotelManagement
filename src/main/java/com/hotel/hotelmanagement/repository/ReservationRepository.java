79139a38cc17c1a36559af4718c0e7965f1ed4ed
RTann95 <rjt224@cornell.edu>
2018-11-18 02:08:56 -0800

    Added comment to explain reasoning for CrudRepo

--- src/main/java/com/hotel/hotelmanagement/repository/ReservationRepository.java
+++ src/main/java/com/hotel/hotelmanagement/repository/ReservationRepository.java
@@ -4,5 +4,6 @@ import com.hotel.hotelmanagement.model.Reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// Do not need all the offerings of JpaRepository, so extending CrudRepository instead.
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {}
