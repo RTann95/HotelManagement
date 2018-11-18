a820d772487614bc1d13b0932c0db3651de211f0
RTann95 <rjt224@cornell.edu>
2018-11-18 02:08:39 -0800

    Added basic test

---
+++ src/test/java/com/hotel/hotelmanagement/controller/HotelManagementControllerTest.java
@@ -0,0 +1,33 @@
+package com.hotel.hotelmanagement.controller;
+
+import com.hotel.hotelmanagement.service.HotelManagementService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HotelManagementControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private HotelManagementService hotelManagementService;
+
+    @Test
+    public void testGetIndex() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, World!"));
+    }
+}
